369980ee3a123ece8a28e7743377b2a7cc7dfb49
tejfi <adamfdeak@gmail.com>
2019-10-09 23:52:38 +0200

    almost done

--- src/main/java/Pages/Workflow.java
+++ src/main/java/Pages/Workflow.java
@@ -1,6 +1,7 @@
 package Pages;
 
 import Util.WebdriverSingleton;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,10 +9,15 @@ import org.openqa.selenium.support.FindBy;
 import java.util.List;
 
 public class Workflow extends BasePage {
+    private WebDriver webDriver;
+
     public Workflow(String route, WebDriver driver) {
         super(route, driver);
     }
 
+
+    @FindBy(id = "project-config-header-name")
+    WebElement projectSettingsHeader;
     @FindBy(xpath = "//div[@class='aui-page-header-main']/h2[@class='project-config-heading']")
     WebElement projectConfigHeading;
     @FindBy(xpath = "//button[@id='project-config-workflow-add-dropdown-trigger']")
@@ -20,13 +26,13 @@ public class Workflow extends BasePage {
     WebElement importFromBundleLink;
     @FindBy(xpath = "//div[@class='aui-page-header-main']//h1[contains(text(),'Select a workflow')]")
     WebElement workFLowSelectHeading;
-    @FindBy(xpath = "//div[@id='workflowBundleListContainer']/div[1]/div[@class='aui-item']/div[@class='workflow-description']/form[@action='/plugins/servlet/wfshare-import/wfshare-import']/input[@value='Select'")
+    @FindBy(xpath = "//div[1]/div[@class='aui-item']/div[@class='workflow-description']/form[@action='/plugins/servlet/wfshare-import/wfshare-import']/input[@value='Select']")
     WebElement selectAgileWorkFlowinput;
     @FindBy(xpath = "//input[@name='wfShareWorkflowName']")
     WebElement workFlowNamingField;
     @FindBy(id = "nextButton")
     WebElement nextButton;
-    @FindBy(xpath = "//section[@class='aui-page-panel-content']/h2[.='Map Workflow Statuses'")
+    @FindBy(xpath = "//strong[.='Workflow has been successfully imported']")
     WebElement mapWorkFlowStatues;
     @FindBy(xpath = "//section[@class='aui-page-panel-content']/h2[.='Preview of import']")
     WebElement previewOfImport;
@@ -36,7 +42,7 @@ public class Workflow extends BasePage {
     WebElement administratorAccesHeading;
     @FindBy(id = "login-form-submit")
     WebElement loginFormSubmit;
-    @FindBy(xpath = "/section[@id='content']//section[@class='aui-page-panel-content']//strong[.='Workflow has been successfully imported']")
+    @FindBy(className = "aui-message")
     WebElement succesFullMessage;
     @FindBy(id = "finishButton")
     WebElement finishButton;
@@ -48,17 +54,31 @@ public class Workflow extends BasePage {
     WebElement submitButton;
     @FindBy(xpath = "//div[@class='project-config-workflow-name']//a")
     List<WebElement> workflows;
+    @FindBy(id = "publish-draft")
+    WebElement publishButton;
+    @FindBy(xpath = "//section[@class='aui-page-panel-content']//h2")
+    WebElement publishH2Tag;
+    @FindBy(id = "workflow-mapping-submit")
+    WebElement associateButton;
+    @FindBy(className = "aui-message aui-message-success success closeable shadowed aui-will-close")
+    WebElement auiMessage;
+
+
+    public boolean checkUserOnTheProjectSettings() {
+
+        return projectSettingsHeader.isDisplayed();
+    }
 
     public boolean checkUserOnTheWorkFlowConfigPage() {
         String actualheadingText = projectConfigHeading.getText();
-        return actualheadingText.equals(WebdriverSingleton.dotEnvLoader().get("Workflows"));
+        return actualheadingText.equals(WebdriverSingleton.dotEnvLoader().get("PROJECT_CONFIG_HEADING"));
     }
 
     public void clickaddWorkFlowButton() {
         addWorkFlowButton.click();
     }
 
-    public boolean bundleOprtionisShown() {
+    public boolean bundleOptionIsShown() {
         return importFromBundleLink.isDisplayed();
 
     }
@@ -77,9 +97,9 @@ public class Workflow extends BasePage {
     }
 
 
-    public void addNameToTheNewWorkflow(String name) {
+    public void addNameToTheNewWorkflow(String workflowName) {
         workFlowNamingField.clear();
-        workFlowNamingField.sendKeys(name);
+        workFlowNamingField.sendKeys(workflowName);
         clickNextButton();
     }
 
@@ -145,4 +165,30 @@ public class Workflow extends BasePage {
 
         return false;
     }
+
+    public void clickPublishButton() {
+        publishButton.click();
+    }
+
+    public String getPublishWorkflowText() {
+        return publishH2Tag.getText();
+    }
+
+    public void clickAssociteButton() {
+        associateButton.click();
+    }
+
+    public boolean auiMEssageIsshown() {
+
+        return auiMessage.isDisplayed();
+
+
+    }
+
+    public void removeWorkflow(String workflowName) {
+        driver.findElement(By.xpath("//a[@title='Remove" + workflowName + "']"));
+    }
+
 }
+
+
--- src/test/java/com/codecool/steps/WorkflowSteps.java
+++ src/test/java/com/codecool/steps/WorkflowSteps.java
@@ -1,12 +1,186 @@
 package com.codecool.steps;
 
+import Pages.Project;
 import Pages.Workflow;
 import Util.WebdriverSingleton;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 import java.net.MalformedURLException;
 
 public class WorkflowSteps {
     private Workflow workflow = new Workflow("plugins/servlet/project-config/WEL/workflows", WebdriverSingleton.getDriver());
+    private Project project = new Project("plugins/servlet/project-config/WEL/summary", WebdriverSingleton.getDriver());
+
     public WorkflowSteps() throws MalformedURLException {
+    }
+
+
+    @Then("Validate user on the PROJECT SETTINGS page")
+    public void validateUserOnThePROJECTSETTINGSPage() {
+        Assert.assertTrue(workflow.checkUserOnTheProjectSettings());
+    }
+
+    @When("Click on Workflows link on the left side bar")
+    public void clickOnWorkflowsLinkOnTheLeftSideBar() {
+        project.goToWorkFlow();
+    }
+
+    @Then("Workflow project config Header will shown")
+    public void workflowProjectConfigHeaderWillShown() {
+        workflow.checkUserOnTheWorkFlowConfigPage();
+    }
+
+    @When("ADD WORKFLOW button is clicked")
+    public void addWORKFLOWButtonIsClicked() {
+        workflow.clickaddWorkFlowButton();
+    }
+
+    @Then("import FROM BUNDLE option is shown")
+    public void importFROMBUNDLEOptionIsShown() {
+        workflow.bundleOptionIsShown();
+    }
+
+    @When("import FROM BUNDLE option is clicked")
+    public void importFROMBUNDLEOptionIsClicked() {
+        workflow.goAndImportFromBundleLink();
+
+    }
+
+    @Then("SELECTION OF WORKFLOWS are shown")
+    public void selectionOFWORKFLOWSAreShown() {
+        workflow.workflowSelectHeading();
+    }
+
+
+    @And("Add name to the Worfklow {string}")
+    public void addNameToTheWorfklow(String workflowName) {
+        workflow.addNameToTheNewWorkflow(workflowName);
+    }
+
+    @When("click NEXT button")
+    public void clickNEXTButton() {
+        workflow.clickNextButton();
+    }
+
+    @Then("Map Workflow Statuses will be shown")
+    public void mapWorkflowStatusesWillBeShown() {
+        if (administratorACCESWillShown()) {
+            ensureUserEnteredValidPassword();
+            clickedTheSubmitButton();
+
+        } else {
+            workflow.checkmapWorkFlowStatues();
+
+        }
+
+    }
+
+    @Then("Preview of Import will shown")
+    public void previewOfImportWillShown() {
+
+    }
+
+    @When("click next button")
+    public void clickNextButton() {
+        clickedTheSubmitButton();
+    }
+
+    @Then("ADMINISTRATOR ACCES will shown")
+    public boolean administratorACCESWillShown() {
+
+        return workflow.administratorAccesHeading();
+    }
+
+    @And("ensure user entered valid password")
+    public void ensureUserEnteredValidPassword() {
+        workflow.addPasswordtoTheField();
+    }
+
+    @And("clicked the Submit button")
+    public void clickedTheSubmitButton() {
+        workflow.clickLoginFormSubmitButton();
+    }
+
+    @Then("Workflow has been successfully imported message will shown")
+    public void workflowHasBeenSuccessfullyImportedMessageWillShown() {
+        workflow.checkSuccesFullyMessage();
+    }
+
+    @And("click the FINISH button")
+    public void clickTheFINISHButton() {
+        workflow.clickFinishButton();
+    }
+
+    @Then("DIALOG MESSAGE shown contains {string}")
+    public void dialogMESSAGEShownContains(String workFlowName) {
+        workflow.dialogHeadingcheck(workFlowName);
+    }
+
+    @And("select task")
+    public void selectTask() {
+        workflow.selectTask();
+    }
+
+    @And("Click submit button")
+    public void clickSubmitButton() {
+        workflow.finishAssign();
+    }
+
+    @And("check Worflow is avaiable")
+    public void checkWorflowIsAvaiable() {
+    }
+
+
+    @When("Select PROJECT SETTINGS bottom of the left side")
+    public void selectPROJECTSETTINGSBottomOfTheLeftSide() {
+        project.goToProjectSettings();
+    }
+
+    @And("check Worflow is avaiable {string}")
+    public void checkWorflowIsAvaiable(String WorkflowName) {
+        workflow.checkWorkflowisCreated(WorkflowName);
+
+    }
+
+    @When("select Easy Agile Scrum Workflow for Jira bundle")
+    public void selectEasyAgileScrumWorkflowForJiraBundle() {
+        workflow.selectWorkFlow();
+
+    }
+
+    @When("click the PUBLISH button")
+    public void clickThePUBLISHButton() {
+        workflow.clickPublishButton();
+    }
+
+
+    @When("click the Associate button")
+    public void clickTheAssociateButton() {
+        workflow.clickAssociteButton();
+    }
+
+    @Then("Your workflows have been published message shown.")
+    public void yourWorkflowsHaveBeenPublishedMessageShown() {
+        Assert.assertTrue(workflow.auiMEssageIsshown());
+    }
+
+    @When("click remove <{string}>  under the actions")
+    public void clickRemoveUnderTheActions(String workflowName) {
+        if (administratorACCESWillShown()) {
+            ensureUserEnteredValidPassword();
+            clickedTheSubmitButton();
+        }
+        workflow.removeWorkflow(workflowName);
+    }
+
+
+    @Then("<{string}>shown will shwon on the left side")
+    public void shownWillShwonOnTheLeftSide(String name) {
+        String actual = workflow.getPublishWorkflowText();
+        Assert.assertEquals(name, actual);
+
     }
 }
