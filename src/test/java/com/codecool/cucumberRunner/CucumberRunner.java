7c911221e1a03551916a00f04079695071652cba
tejfi <adamfdeak@gmail.com>
2019-10-14 16:26:14 +0200

    cucumber json

--- src/test/java/com/codecool/cucumberRunner/CucumberRunner.java
+++ src/test/java/com/codecool/cucumberRunner/CucumberRunner.java
@@ -7,6 +7,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = ".//src/test/java/com/codecool/features",
         glue = {"com.codecool.steps"},
-        plugin = {"html:target/cucumber-html-report", "json:target/cucumber-json-report.json"})
+        plugin = {"html:target/cucumber-html-report", "json:target/cucumber-json-report"})
 public class CucumberRunner {
 }
